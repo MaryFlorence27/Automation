a757b8919bb8d8726f3ea9f45b1ca7bd50f91a87
MaryFlorence27 <marymca27@gmail.com>
2023-06-23 14:46:54 +0530

    CRS-123 committing helloworld changes

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -1,9 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("This is my first github commit integrating with jenkinsssss");
-=======
+
         System.out.println("This is my first github commit integrating with jenkins");
->>>>>>> master
     }
 }
