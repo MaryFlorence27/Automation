3addf4fabcb4c2d7a33454540b1955c39815e749
MaryFlorence27 <136088035+MaryFlorence27@users.noreply.github.com>
2023-06-26 18:07:18 +0530

    eclipse message

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first github commit integrating with jenkins");
         System.out.println("This is commit from feature into master");
-        System.out.println("This Demo is for git rebase from Intellij");
+        System.out.println("This Demo is for git rebase from eclipse");
     }
 }
